8e98df439c1da1cc863bf1341279647ba129f3f3
redsox1345Rubier <genki999@mail.ru>
2023-04-30 22:12:35 +0300

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,8 @@
 function compareArrays(arr1, arr2) {
-  
+	return arr1.length === arr2.length && arr1.every((v, i) => v === arr2[i]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
-}
+
+	return users.filter(user => gender === user.gender).map(user => user.age).reduce((summator, age, index, userAges) => summator + age / userAges.length, 0);
+	}
